ea72f222c4c63a7397f017403f5877a006b6cbba
MarvinTheCoder <marvinmusambi39@gmail.com>
2021-06-10 19:51:47 +0300

    change

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '/../client/build')));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 // });
-
+//
 /** Require the routes **/
 const userScoreRoutes = require('./routes/userscore')
 
